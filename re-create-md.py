3780bd7a3a5355ce213c46f227dd93fedeeed2b7
Harvey <harvey@HarveydeMacBook-Air.local>
2025-09-04 20:47:27 +0800

    天支持

--- re-create-md.py
+++ re-create-md.py
@@ -3,12 +3,16 @@
 # 重新创建 README.md 文件
 #
 
+lastDayDate = open('day/last.date').read()
+lastDayRank = open("day/%s.md"%(lastDayDate), 'r').read()
 lastWeekDate = open('week/last.date').read()
 lastWeekRank = open("week/%s.md"%(lastWeekDate), 'r').read()
 lastMonthDate = open('month/last.date').read()
 lastMonthRank = open("month/%s.md"%(lastMonthDate), 'r').read()
 
 datas = {
+    '{last-day-date}': lastDayDate,
+    '{last-day-rank}': lastDayRank,
     '{last-week-date}': lastWeekDate,
     '{last-week-rank}': lastWeekRank,
     '{last-month-date}': lastMonthDate,    
